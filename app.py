c70ce3f093000207389e34ad9ecaa8b5b14f184d
Valentine Solonechnyi <valentinesolo@gmail.com>
2022-04-09 17:56:50 +0200

    remove  run command

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from db.config import engine, Base
 app = FastAPI()
 app.include_router(router)
 
-if settings.env_name == 'development':
-    if __name__ == '__main__':
-        uvicorn.run("app:app", port=8001, host='127.0.0.1')
-
 # @app.on_event("startup")
 # async def startup():
 #     # create db tables
