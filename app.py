e194f3225721deb33031f2334828dc20bf7a8e11
Valentine Solonechnyi <valentinesolo@gmail.com>
2022-04-09 17:55:44 +0200

    remove db drop/init

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ if settings.env_name == 'development':
     if __name__ == '__main__':
         uvicorn.run("app:app", port=8001, host='127.0.0.1')
 
-@app.on_event("startup")
-async def startup():
-    # create db tables
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+# @app.on_event("startup")
+# async def startup():
+#     # create db tables
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/")
